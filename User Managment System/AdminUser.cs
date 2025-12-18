804b871e3a7d178a1390be60ec7460d2d8145ed3
Mrjackabasi <mr.jack1174@gmail.com>
2025-12-18 13:24:35 +0330

    add class base user and Derived Classes

---
+++ User Managment System/AdminUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace User_Managment_System
+{
+    public class AdminUser : User
+    {
+       
+
+        public int PremissionLevel { get; set; }
+        public AdminUser(int id, string? fullName, string? email,int premissionLevel) : base(id, fullName, email)
+        {
+            PremissionLevel = premissionLevel;
+        }
+        public void ManageUsers()
+        {
+            Console.WriteLine($"{FullName}Manageing Users .");
+        }
+        public override string GetUserInfo()
+        {
+            return $"[ADMIN] {FullName} | EMAIL{Email}| PremissonLevel {PremissionLevel}";
+        }
+
+
+
+    }
+
+
+
+}
+
+
+
+
---
+++ User Managment System/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace User_Managment_System
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public bool IsActive { get; set; }
+
+        public User(int id, string? fullName, string? email)
+        {
+            Id = id;
+            FullName = fullName;
+            Email = email;
+            IsActive = true;
+        }
+        public void Login()
+        {
+            Console.WriteLine($"{FullName}you are login");
+        }
+        public void Logout()
+        {
+            Console.WriteLine($"you are logout");
+        }
+        public virtual string GetUserInfo()
+        {
+            return $"User : {FullName} - Email : {Email}";
+        }
+    }
+}
